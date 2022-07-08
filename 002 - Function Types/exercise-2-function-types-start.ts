ff67ebe6dada7184a1ccad2fa7b8126d48f9de8a
leo_feola <feola.leopoldo@gmail.com>
2022-07-08 11:41:27 +0200

    esercizi 1-6

--- 002 - Function Types/exercise-2-function-types-start.ts
+++ 002 - Function Types/exercise-2-function-types-start.ts
@@ -4,7 +4,7 @@
 
 // Add types to this function declaration.
 
-function doubleThePopulation(value) {
+function doubleThePopulation(value: number): number {
     return value * 2;
 }
 
@@ -12,21 +12,23 @@ function doubleThePopulation(value) {
 
 doubleThePopulation(5);
 
-doubleThePopulation("6.12");
+//doubleThePopulation("6.12");
 
 doubleThePopulation(8_526);
 
-doubleThePopulation(false);
+//doubleThePopulation(false);
 
 // Alter this function so the `language2` parameter is optional.
 // Hint: Check `language2` is not `undefined` before passing it to `console.log()`.
 
-function languagesSpoken(country: string, language1: string, language2: string): void {
+function languagesSpoken(country: string, language1: string, language2?: string): void {
     console.log(`The languages spoken in ${country} are:`);
 
     console.log(language1);
 
-    console.log(language2);
+    if (typeof language2 === "string") {
+        console.log(language2);
+    }
 }
 
 languagesSpoken("Colombia", "Spanish", "English");
@@ -37,4 +39,4 @@ languagesSpoken("New Zealand", "English", "Māori");
 
 // ----
 
-export {};
+export { };
